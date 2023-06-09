f8f184ac1b86e0277f20544ec28829458102238c
gmldyddl123 <gmldyddl123@gmail.com>
2023-06-09 18:52:09 +0900

    Revert "몬스터 반응시 바라보는방향 조정"
    This reverts commit 73ba80732900e431897e77c5db7d16d8f82506c4.

--- Assets/Script/Monster.cs
+++ Assets/Script/Monster.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 public class Monster : MonoBehaviour
@@ -10,27 +9,19 @@ public class Monster : MonoBehaviour
     public float speed = 2.0f; 
     Player player;
     float velocity;
-    private Quaternion targetRotation;
-    public float rotationSpeed = 5.0f;
-
 
     bool tagetOn = false;
     private void Awake()
     {
         player = FindObjectOfType<Player>();
         target = player.transform;
-        
     }
 
 
     private void Update()
     {
-        if (tagetOn)
-        {
-
-            
+        if(tagetOn)
             MoveToTarget();
-        }
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -56,29 +47,15 @@ public class Monster : MonoBehaviour
 
     public void MoveToTarget()
     {
-        Vector3 direction = target.position - transform.position;
-        direction.y = 0;
-        targetRotation = Quaternion.LookRotation(direction);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
+        
         direction = (target.position - transform.position).normalized;
-       
 
         velocity =  speed * Time.deltaTime;
        
         transform.position = new Vector3(transform.position.x + (direction.x * velocity),
                                                transform.position.y + (direction.y * velocity),
                                                   transform.position.z + (direction.z * velocity));
-        
     }
-    //public void LookAtTarget()
-    //{
-    //    Vector3 direction = target.position - transform.position;
-    //    direction.y = 0; 
-    //    targetRotation = Quaternion.LookRotation(direction);
-    //    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); 
-    //}
-
 }
 
 
